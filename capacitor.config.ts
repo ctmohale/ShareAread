c1ea1529be5d5e91e31312b3a2c42caf4d79e2b5
ctmohale <60016492+ctmohale@users.noreply.github.com>
2023-07-16 09:40:16 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'shere-a-read',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
